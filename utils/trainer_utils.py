d82bb6e70297005e6cea34eec007f1d8aacd7590
timohromadka <timo.hromadka@gmail.com>
2023-12-22 01:36:31 +0100

    Fix code to correctly setup lightning training

--- utils/trainer_utils.py
+++ utils/trainer_utils.py
@@ -3,95 +3,73 @@ import wandb
 
 import torch
 import torch.nn.functional as F
-import pytorch_lightning
+import pytorch_lightning as pl
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.callbacks import RichProgressBar, LearningRateMonitor
 from pytorch_lightning.loggers import WandbLogger
 
-from ..callbacks.training_dynamics_callback import DataMapLightningCallback
+import sys
+sys.path.append("..")
+from callbacks.training_dynamics_callback import DataMapLightningCallback
 
 
-def train_model(args, model, data_module, wandb_logger=None):
-	"""
-	Return 
-	- Pytorch Lightning Trainer
-	- checkpoint callback
-	"""
+def train_model(args, model, data_module, train_unshuffled_loader, wandb_logger=None):
+    """
+    Return 
+    - Pytorch Lightning Trainer
+    - checkpoint callback
+    - datamap callback
+    """
+    pl.seed_everything(args.seed, workers=True)
 
-	##### Train
-	if args.saved_checkpoint_name:
-		wandb_artifact_path = f'th716/low-data/{args.saved_checkpoint_name}'
-		print(f"\nDownloading artifact: {wandb_artifact_path}...")
+    mode_metric = 'max' if args.metric_model_selection == 'balanced_accuracy' else 'min'
+    
+    checkpoint_callback = ModelCheckpoint(
+        #monitor=f'valid/{args.metric_model_selection}',
+        monitor=f'{args.metric_model_selection}',
+        mode=mode_metric,
+        save_last=True,
+        verbose=True,
+        dirpath=args.checkpoint_dir,
+        filename='{epoch}-{step}-{valid_loss:.2f}',
+        save_top_k=args.checkpoint_freq
+    )
 
-		artifact = wandb.use_artifact(wandb_artifact_path, type='model')
-		artifact_dir = artifact.download()
-		model_checkpoint = torch.load(os.path.join(artifact_dir, 'model.ckpt'))
-		weights = model_checkpoint['state_dict']
-		print("Artifact downloaded")
+    datamap_callback = DataMapLightningCallback(
+        train_unshuffled_loader,
+        args.model,
+        args.dataset,
+        outputs_to_probabilities=lambda x: F.softmax(x[0]),
+        sparse_labels=True,
+    )
+    callbacks = [checkpoint_callback, RichProgressBar(), datamap_callback]
 
-		if args.load_model_weights:
-			print(f"\nLoading pretrained weights into model...")
-			missing_keys, unexpected_keys = model.load_state_dict(weights, strict=False)
-			print(f"Missing keys: \n")
-			print(missing_keys)
+    if args.patience_early_stopping and not args.train_on_full_data:
+        callbacks.append(EarlyStopping(
+            # monitor=f'valid/{args.metric_model_selection}',
+            monitor=f'{args.metric_model_selection}',
+            mode=mode_metric,
+            patience=args.patience_early_stopping,
+        ))
+    callbacks.append(LearningRateMonitor(logging_interval='epoch'))
 
-			print(f"Unexpected keys: \n")
-			print(unexpected_keys)
-
-	mode_metric = 'max' if args.metric_model_selection=='balanced_accuracy' else 'min'
-	checkpoint_callback = ModelCheckpoint(
-		monitor=f'valid/{args.metric_model_selection}',
-		mode=mode_metric,
-		save_last=True,
-		verbose=True
-	)
+    trainer = pl.Trainer(
+        max_epochs=args.epochs,
+        gradient_clip_val=args.gradient_clip_val,
+        logger=wandb_logger,
+        log_every_n_steps=args.logging_interval,
+        val_check_interval=args.val_check_interval,
+        callbacks=callbacks,
+        accelerator=args.accelerator,
+        devices=args.num_gpus,
+        precision=args.precision,
+        # detect_anomaly=True,
+        detect_anomaly=False,
+        overfit_batches=args.overfit_batches,
+        deterministic=args.deterministic,
+    )
  
-    #  return DataMapCallback(train_unshuffled.batch(batch_size),
-    #                        train_unshuffled_raw,
-    #                        model_name,
-    #                        dataset_name,
-    #                         outputs_to_probabilities=lambda x: tf.nn.softmax(x[0]), # Model outputs a tuple, where the logits are at the first index
-    #                         sparse_labels=True,
-    #                         gold_labels_probabilities=gold_labels_probabilities)
- 
-	datamap_callback = DataMapLightningCallback(
-		data_module,
-		args.model_name,
-		args.dataset_name,
-  		outputs_to_probabilities=lambda x: F.softmax(x[0]), # Model outputs a tuple, where the logits are at the first index
-		sparse_labels=True, # conversion to one hot encoding necessary
-	)
-	callbacks = [checkpoint_callback, RichProgressBar(), datamap_callback]
+    trainer.fit(model, data_module)
 
-	if args.patience_early_stopping and args.train_on_full_data==False:
-		callbacks.append(EarlyStopping(
-			monitor=f'valid/{args.metric_model_selection}',
-			mode=mode_metric,
-			patience=args.patience_early_stopping,
-		))
-	callbacks.append(LearningRateMonitor(logging_interval='step'))
-
-	pl.seed_everything(args.seed_training, workers=True)
-	trainer = pl.Trainer(
-		# Training
-		max_steps=args.max_steps,
-		gradient_clip_val=2.5,
-
-		# logging
-		logger=wandb_logger,
-		log_every_n_steps = 1,
-		val_check_interval = args.val_check_interval,
-		callbacks = callbacks,
-
-		# miscellaneous
-		accelerator="auto",
-		devices="auto",
-		detect_anomaly=True,
-		overfit_batches=args.overfit_batches,
-		deterministic=args.deterministic,
-	)
-	# train
-	trainer.fit(model, data_module)
-	
-	return trainer, checkpoint_callback, datamap_callback
+    return trainer, checkpoint_callback, datamap_callback
