6a95a6f13c2a8b2fe4ca17cd838c321ccb3004d5
timohromadka <timo.hromadka@gmail.com>
2023-12-22 14:21:10 +0100

    Add try statement to deal with wandb not finishing on crashed runs.

--- run_experiment.py
+++ run_experiment.py
@@ -17,48 +17,56 @@ logger = logging.getLogger('Run Experiment Pipeline for Datamaps!')
 
 
 def process_results(args):
+    # If anything needs to be processed after training/testing, it should go here
+    # IDEAS:
+    # - perhaps some training dynamics statistics?
     return
 
 def main():
-    # ================================
-    # SET UP ARGS
-    # ================================
-    args = parser.parse_args()
-    project_name = "L46_datamaps"
+    try:
+        # ================================
+        # SET UP ARGS
+        # ================================
+        args = parser.parse_args()
+        project_name = "L46_datamaps"
+        
+        # ================================
+        # CONFIGURE WANDB
+        # ================================
+        if args.disable_wandb:
+            os.environ['WANDB_MODE'] = 'disabled'
+        wandb.init(project=project_name, config=args)
     
-    # ================================
-    # CONFIGURE WANDB
-    # ================================
-    if args.disable_wandb:
-        os.environ['WANDB_MODE'] = 'disabled'
-    wandb.init(project=project_name, config=args)
-  
-    wandb_logger = create_wandb_logger(args, project_name)
-    # wandb.run.name = f"{get_run_name(args)}_{args.suffix_wand_run_name}_{wandb.run.id}"
-    wandb.run_name = args.wandb_run_name
+        wandb_logger = create_wandb_logger(args, project_name)
+        # wandb.run.name = f"{get_run_name(args)}_{args.suffix_wand_run_name}_{wandb.run.id}"
+        wandb.run.name = args.wandb_run_name
 
-    # ================================
-    # FETCH MODEL
-    # ================================
-    model = get_model(args)
 
-    # ================================
-    # FETCH DATASET
-    # ================================
-    train_loader, train_unshuffled_loader, val_loader, test_loader = get_dataloaders(args)
-    data_module = CustomDataModule(train_loader, val_loader, test_loader)
-    
-    # ================================
-    # UNDERGO TRAINING
-    # ================================
-    trainer, checkpoint_callback, datamap_callback = train_model(
-        args, model, data_module, train_unshuffled_loader, wandb_logger
-    )
-    
-    process_results(args)
-    
-    # Done!
-    wandb.finish()
+        # ================================
+        # FETCH MODEL
+        # ================================
+        model = get_model(args)
+
+        # ================================
+        # FETCH DATASET
+        # ================================
+        train_loader, train_unshuffled_loader, val_loader, test_loader = get_dataloaders(args)
+        data_module = CustomDataModule(train_loader, val_loader, test_loader)
+        
+        # ================================
+        # UNDERGO TRAINING
+        # ================================
+        trainer, checkpoint_callback, datamap_callback = train_model(
+            args, model, data_module, train_unshuffled_loader, wandb_logger
+        )
+        
+        process_results(args)
+        
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+    finally:
+        wandb.finish()
 
 if __name__ == "__main__":
     main()
