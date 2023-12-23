57f137884158fc96658a78912c6b87f56180b5ff
timohromadka <timo.hromadka@gmail.com>
2023-12-23 01:51:35 +0100

    (Updated) Add working training dynamics collection after each epoch and json saving to wandb after training. Previous commit didn't save

--- callbacks/training_dynamics_callback.py
+++ callbacks/training_dynamics_callback.py
@@ -1,45 +1,39 @@
 import os
 import json
 import numpy as np
+import wandb
+
 import torch
+import torch.nn.functional as F
 from pytorch_lightning.callbacks import Callback
 from tqdm import tqdm
 
+# TODO
+# - fix glitchy contiguous tqdm progress bar?
 class DataMapLightningCallback(Callback):
-    def __init__(self, dataloader, model_name, dataset_name, outputs_to_probabilities=None, sparse_labels=False, gold_labels_probabilities=None):
+    def __init__(self, dataloader, model_name, dataset_name, outputs_to_probabilities=lambda x, dim: F.softmax(x, dim), sparse_labels=False, gold_labels_probabilities=None, run_name='default_run_name'):
         self.dataloader = dataloader
-        self.model_name = model_name
-        self.dataset_name = dataset_name
         self.outputs_to_probabilities = outputs_to_probabilities
         self.sparse_labels = sparse_labels
         self.gold_labels_probabilities = gold_labels_probabilities
+        self.run_name = run_name
+        self.training_dynamics = {}
+        
 
-    def on_epoch_end(self, trainer, pl_module):
+    def on_train_epoch_end(self, trainer, pl_module):
         print(f'\nEpoch has ended! Now calculating gold labels probabilities.\n')
 
         gold_label_probabilities = []
         pl_module.eval()
         with torch.no_grad():
-            for batch in self.dataloader:
+            for batch in tqdm(self.dataloader, desc='Calculating gold label probabilities'):
                 x, y = batch
+                x = x.to(pl_module.device) # lightning will not handle this automatically
                 probabilities = pl_module(x)
 
-                if self.outputs_to_probabilities is not None:
-                    probabilities = self.outputs_to_probabilities(probabilities)
-
-                if self.sparse_labels:
-                    y = torch.nn.functional.one_hot(y, num_classes=probabilities.shape[-1])
-
-                if y.dim() == 1:  # Binary labels
-                    probabilities = probabilities.squeeze()
-                    y = y.squeeze()
-                    batch_gold_label_probabilities = torch.where(y == 0, 1 - probabilities, probabilities)
-                elif y.dim() == 2:  # Multiclass labels
-                    if not torch.all(torch.sum(y == 1, dim=-1) == 1):
-                        raise ValueError('DataMapLightningCallback does not support multi-label classification')
-                    batch_gold_label_probabilities = probabilities[y.bool()].cpu().numpy()
-                else:
-                    raise ValueError('y must be 1D for binary classification or 2D for multiclass.')
+                probabilities = self.outputs_to_probabilities(probabilities, dim=1)
+                
+                batch_gold_label_probabilities = probabilities[torch.arange(probabilities.shape[0]), y].cpu().numpy()
 
                 gold_label_probabilities.append(batch_gold_label_probabilities)
 
@@ -49,70 +43,44 @@ class DataMapLightningCallback(Callback):
         else:
             self.gold_labels_probabilities = np.concatenate([self.gold_labels_probabilities, gold_label_probabilities[..., None]], axis=-1)
 
-        self.save_training_dynamics_to_json()
-
-    def convert_numpy(self, obj):
-        if isinstance(obj, torch.Tensor):
-            return obj.cpu().numpy()
-        return obj
-
-    def save_training_dynamics_to_json(self):
-        print(f'Saving training dynamics at the end of the epoch.')
-        gold_label_probs = self.gold_labels_probabilities
-        confidence = self.confidence
-        variability = self.variability
-        correctness = self.correctness
-        forgetfulness = self.forgetfulness
-
-        json_dict = {}
-        for i, example in enumerate(tqdm(self.dataloader.dataset, desc='Saving training indexes along with training dynamics to json')):
-            idx = self.convert_numpy(example["idx"])
-            json_dict[idx] = {
-                "gold_label_probs": self.convert_numpy(gold_label_probs[i]),
-                "confidence": self.convert_numpy(confidence[i]),
-                "variability": self.convert_numpy(variability[i]),
-                "correctness": self.convert_numpy(correctness[i]),
-                "forgetfulness": self.convert_numpy(forgetfulness[i]),
-            }
-
-        directory_path = f'training_dynamics/{self.dataset_name}'
-        if not os.path.exists(directory_path):
-            os.makedirs(directory_path)
-
-        json_file_name = f'{directory_path}/{self.model_name}.json'
-        print(f'\nSaving training dynamics to {json_file_name}.')
+    def on_train_end(self, trainer, pl_module):
+        print(f'\nTraining has ended! Preparing and uploading training dynamics to WandB.\n')
+        
+        for idx, cur_gold_label_probs in tqdm(enumerate(self.gold_labels_probabilities), desc='Calculating training dynamics from gold labels probabilities.'):
+            current_dynamics = {}
+            current_dynamics['gold_label_probs'] = [float(value) for value in cur_gold_label_probs.tolist()]  # Convert each value to float
+            current_dynamics['confidence'] = self.confidence(cur_gold_label_probs)
+            current_dynamics['variability'] = self.variability(cur_gold_label_probs)
+            current_dynamics['correctness'] = self.correctness(cur_gold_label_probs)
+            current_dynamics['forgetfulness'] = self.forgetfulness(cur_gold_label_probs)
+            self.training_dynamics[idx] = current_dynamics
+            
+        # Save to JSON
+        json_file_name = f'{self.run_name}_training_dynamics.json'
         with open(json_file_name, 'w') as file:
-            json.dump(json_dict, file, ensure_ascii=False, indent=4)
+            json.dump(self.training_dynamics, file, ensure_ascii=False, indent=4)
 
-    @property
-    def confidence(self):
-        if self.gold_labels_probabilities is None:
-            return None
-        return np.mean(self.gold_labels_probabilities, axis=-1)
+        # Upload to WandB
+        wandb.save(json_file_name)
 
-    @property
-    def variability(self):
-        if self.gold_labels_probabilities is None:
-            return None
-        return np.std(self.gold_labels_probabilities, axis=-1)
+    @staticmethod
+    def confidence(gold_label_probs):
+        return np.mean(gold_label_probs)
 
-    @property
-    def correctness(self):
-        if self.gold_labels_probabilities is None:
-            return None
-        return np.mean(self.gold_labels_probabilities > 0.5, axis=-1)
+    @staticmethod
+    def variability(gold_label_probs):
+        return np.std(gold_label_probs)
 
-    @property
-    def forgetfulness(self):
-        if self.gold_labels_probabilities is None:
-            return None
-        epochs = np.arange(self.gold_labels_probabilities.shape[1])
+    @staticmethod
+    def correctness(gold_label_probs):
+        # TODO
+        # UPDATE TO FOLLOW NUMBER OF CLASSES!!!! Not 0.5!!! it's not binary classification
+        return np.mean(gold_label_probs > 0.5)
 
-        def calculate_slope(y_values):
-            if len(y_values) == 1:
-                return 0
-            m, _ = np.polyfit(epochs, y_values, 1)
-            return m
-
-        return np.apply_along_axis(calculate_slope, 1, self.gold_labels_probabilities)
-
+    @staticmethod
+    def forgetfulness(gold_label_probs):
+        epochs = np.arange(len(gold_label_probs))
+        if len(gold_label_probs) == 1:
+            return 0
+        m, _ = np.polyfit(epochs, gold_label_probs, 1)
+        return m
