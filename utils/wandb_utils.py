f707928ce8da4bedd9cf257dc55a03609bf16fab
timohromadka <timo.hromadka@gmail.com>
2023-12-24 00:47:12 +0100

    Add wandb utils for fetching runs and training dynamics from wandb

--- utils/wandb_utils.py
+++ utils/wandb_utils.py
@@ -1,21 +1,8 @@
+import json
 import wandb
 
+import pandas as pd
 from pytorch_lightning.loggers import WandbLogger
-
-# def get_run_name(args):
-# 	if args.model=='dnn':
-# 		run_name = 'mlp'
-# 	elif args.model=='dietdnn':
-# 		run_name = 'mlp_wpn'
-# 	else:
-# 		run_name = args.model
-
-# 	if args.sparsity_type=='global':
-# 		run_name += '_SPN_global'
-# 	elif args.sparsity_type=='local':
-# 		run_name += '_SPN_local'
-
-# 	return run_name
 
 def create_wandb_logger(args, project_name):
 	wandb_logger = WandbLogger(
@@ -32,3 +19,65 @@ def create_wandb_logger(args, project_name):
 	wandb_logger.experiment.config.update(args)	  # add configuration file
 
 	return wandb_logger
+
+
+def flatten_config(config):
+    flat_config = {}
+    for key, value in config.items():
+        if isinstance(value, dict):
+            # For nested dictionaries, you can either flatten further or handle specific cases
+            # Here, assuming a shallow nested structure
+            for sub_key, sub_value in value.items():
+                flat_config[f"{key}_{sub_key}"] = sub_value
+        else:
+            flat_config[key] = value
+    return flat_config
+
+def get_metrics(run, metric_names):
+    metrics = {}
+    for name in metric_names:
+        # Fetching the metric's summary might vary depending on how it's logged
+        # Using '.get()' to avoid KeyError if the metric is not found
+        metrics[name] = run.summary.get(name)
+    return metrics
+
+def get_runs_dataframe(project_name, entity_name='l46_datamaps'):
+    api = wandb.Api()
+    runs = api.runs(f"{entity_name}/{project_name}")
+        
+    metric_names = ["test_recall", "test_loss", "val_loss", "test_precision", "test_f1", "test_acc", "val_loss", "val_acc", "epoch", "train_acc_epoch", "train_loss_epoch", "lr-Adam"]
+    runs_data = []
+    for run in runs:
+        run_data = {
+            'id': run.id,
+            'name': run.name,
+            'state': run.state,
+        }
+        
+        run_data.update(flatten_config(run.config))
+        run_data.update(get_metrics(run, metric_names))
+        runs_data.append(run_data)
+
+    return pd.DataFrame(runs_data)
+
+def get_run_id_from_run_name(run_name, run_df):
+    return run_df.loc[run_df['wandb_run_name'] == run_name, 'id'].iloc[0]
+
+def get_training_dynamics(api, project_name, entity_name, run_id, run_name):
+    run = api.run(f"{entity_name}/{project_name}/{run_id}")
+    json_file_name = f'training_dynamics/{run_name}_training_dynamics.json'
+    file_object = run.file(json_file_name).download(replace=True)
+    training_dynamics = json.load(file_object)
+    
+    zipped_values = zip(*[(values['gold_label_probs'], values['confidence'], values['variability'], values['correctness'], values['forgetfulness'])
+                      for values in training_dynamics.values()])
+
+    return zipped_values
+
+def get_training_dynamics_from_run_name(project_name, entity_name, run_name):
+    run_df = get_runs_dataframe(project_name, entity_name)
+    run_id = get_run_id_from_run_name(run_name, run_df)
+    api = wandb.Api()
+    training_dynamics = get_training_dynamics(api, project_name, entity_name, run_id, run_name)
+    
+    return training_dynamics
