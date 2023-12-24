afc5f424ba8055e0b13ead3f73b0f62c996c560f
evanrex <evangeorgerex@gmail.com>
2023-12-24 19:47:01 +0000

    wip
    Successful run on dev-gpu-acs server, so freezing new requirements file and saved run command in bash script.

---
+++ scripts_experiments/run_exp.sh
@@ -0,0 +1,7 @@
+python3 run_experiment.py \
+    --model resnet \
+    --model_size small \
+    --dataset cifar10 \
+    --epochs 2 \
+    --track_training_dynamics \
+	--tags 'bad' 'datamap dataloader debug' \  
